aeca01158223fb87610418fa57526975c14f596b
BKutlar <91670142+BKutlar@users.noreply.github.com>
2021-12-28 16:26:38 +0100

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="flex justify-center items-center h-16
+        bg-green-500 text-white">
+            <p>Copyright © 2021 PAPAYES All rights reserved.</p>
+        </div>
+    )
+}
+
+export default Footer; 
